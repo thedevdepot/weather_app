f04134ece50cefb01e7e68bfec0f39f472566429
Python Mega Course <tonyringertx@gmail.com>
2024-10-19 20:57:47 -0500

    init commit

---
+++ .venv/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("The Weather for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forecast Days", min_values=1, max_value=5,
+                 help="Select the number of forcast days.")
+option = st.selectbox("Select data to view",
+                      "Temperature", "Sky")
+st.subheader(f"{option} for the next {days} days in {place}")
