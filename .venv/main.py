d9466166d8fc63bfcde7ea850a52cb25713e5f7e
Python Mega Course <tonyringertx@gmail.com>
2024-10-21 17:58:45 -0500

    added chart

--- .venv/main.py
+++ .venv/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+
 
 st.title("The Weather for the Next Days")
 place = st.text_input("Place: ")
@@ -7,3 +9,9 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+dates = ["2022-25-10", "2022-25-11", "2022-25-12"]
+temperatures = [10, 11, 15]
+
+figure = px.line(x=dates, y=temperatures, labels={"x": "dates", "y": "temperature (C)"})
+st.plotly_chart(figure)
